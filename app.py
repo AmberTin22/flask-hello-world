7fb7270eaafba1f6e4830ee789f97cee1c3b2067
Amber Tin <amti9774@colorado.edu>
2024-03-26 03:56:19 +0000

    flaskDB

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World from Amber in 3308.'
+
+# connection to database
+@app.route('/db_test')
+def testing():
+	conn = psycopg2.connect("postgres://tin_db_user:tTiToULPV8Lk0GywTYolmJYineD40MUb@dpg-co0ekkol5elc738o47p0-a/tin_db")
+	conn.close
+    return 'Database Connection Successful'
+
+
+
