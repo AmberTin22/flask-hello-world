ee40a4604a6ae8f46d1c58ed84503f31942dd3eb
Amber Tin <amti9774@colorado.edu>
2024-03-26 05:21:30 +0000

    bskball db

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Amber in 3308.'
 
+
 # connection to database
 @app.route('/db_test')
 def testing():
@@ -15,4 +16,18 @@ def testing():
 	return "Database Connection Successful"
 
 
-
+@app.route('/db_create')
+def testing():
+	conn = psycopg2.connect("postgres://tin_db_user:tTiToULPV8Lk0GywTYolmJYineD40MUb@dpg-co0ekkol5elc738o47p0-a/tin_db")
+	cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+	conn.commit()
+	conn.close()
+	return "Basketball Table Successfully Created"
